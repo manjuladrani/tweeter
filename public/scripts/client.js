58b172518e5e374d6ddda06d12187bcbc8a704f4
manjula <manjulad.311@gmail.com>
2021-10-07 04:12:33 +0000

    fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,15 +4,26 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(() => { // same as $(document).ready(() => {})
+
+    function loadTweets() {
+        console.log("inside load tweets");
+        $.ajax({
+            url: '/tweets',
+            method: 'GET',
+            datatype: "json",
+            success: (tweets) => {
+                //console.log("data:", tweets);
+                tweets.reverse();
+                renderTweets(tweets);
+            }
+        })
+    }
+    loadTweets();
+
+
     const createTweetElement = function (tweet) { 
-
-    //    const $article = $("<article>").addClass("exist-tweet");
-    //    const $header = $("<header>").addClass("head");
-    //    const $photo = $("<div>").addClass("photo");
-    //    const $
-
-
-    let markup = `
+        
+        let markup = `
          <article class="exist-tweet">
           <header >
             <div class="photo">
@@ -40,82 +51,19 @@ $(() => { // same as $(document).ready(() => {})
         return markup;
 
     };
-
-
- 
-    //   console.log($tweet);
-    //   $(`<article class="tweet">Hello world</article>`);
-    // const $title = $("<h1>").text(blog.title);
-    // const $body = $("<p>").text(blog.body);
-    // const $authorId = $("<p>").text(`author id: ${blog.userId}`) 
-
-    //const $blog = $("<div>").addClass("blog-post");
-    // $article.append($header, $body, $footer);   
-    // return $article;
    
-    // const tweetData = {
-    //     "user": {
-    //     "name": "Newton",
-    //     "avatars": "https://i.imgur.com/73hZDYK.png",
-    //         "handle": "@SirIsaac"
-    //     },
-    //     "content": {
-    //         "text": "If I have seen further it is by standing on the shoulders of giants"
-    //     },
-    //     "created_at": 1461116232227
-    // }
-
-    // const $tweet = createTweetElement(tweetData);
-
-    // // Test / driver code (temporary)
-    // console.log($tweet); // to see what it looks like
-    // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-    const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        }
-      ]
     
     const renderTweets = function(tweets) {
       // loops through tweets
       // calls createTweetElement for each tweet
       // takes return value and appends it to the tweets container
+      $('#tweets-container').empty();
       for( let tweet of tweets) {
-        
         $('#tweets-container').append(createTweetElement(tweet));
-
-
-
-        }
+      }
 
     }
     
-    // const createTweetElement = function(tweet) {
-    //   let $tweet = /* Your code for creating the tweet element */
-    //   // ...
-    //   return $tweet;
-    // }
-    
-    renderTweets(data);
     
     $("#tweet-form").submit(function (event) {
         console.log('event');
@@ -124,8 +72,38 @@ $(() => { // same as $(document).ready(() => {})
         const serializedData = $(this).serialize();
 
         $.post("/tweets", serializedData, (response) => {
-            console.log(response);
+            console.log("before calling");
+            loadTweets();
+            console.log("Tweet");
         });
     })
+
+
+
+
+
+    /*The loadTweets function is defined here: It is an ajax GET request and takes the tweets as a JSON object and places them in the body of the response*/
+
     
+
+    // const charCount = $(this).find("textarea")[0].value.length;
+    // if (charCount === 0) {
+    //     console.log($('#error') + "You need letters to tweet");
+    // } else if (charCount > 140){
+    //     console.log($('error') + "Too many letters");
+    // } else {
+    //     $.ajax({
+    //         url:'/tweets',
+    //         method:'POST',
+    //         data: $(this).serialize(), 
+    //         success: () => {
+    //           $('.new-tweet').val('');
+              
+    //           $('.counter').text('140');
+    //           loadTweets();  
+    //         }    
+    //       });
+
+    // }
+
 });  
